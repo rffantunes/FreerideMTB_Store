cb9c0c241ddc9d1238ad1c88e79281b97c439c9e
Renato Filipe Farinha Antunes <rffantunes@hotmail.com>
2017-05-04 23:51:37 +0100

    Adicionar arquivos de projeto.

---
+++ FreerideMTB_Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FreerideMTB_Store.Startup))]
+namespace FreerideMTB_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
